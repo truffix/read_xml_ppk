147039f82aa72d3c560a083d8882b6875716d49f
atrufanov <atryfv@gmail.com>
2023-02-27 16:54:01 +0300

    new project

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ import customtkinter
 import threading
 
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
 
 class App(customtkinter.CTk):
